a951ace2391729b4aa356f4a8bf03fe161a3bd9e
HasanHadimu <hasanbashri302@gmail.com>
2025-01-20 00:29:29 +0900

    first commit

---
+++ app/Livewire/ChatComponent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+use App\Models\User;
+use App\Models\Message;
+use Livewire\Component;
+
+class ChatComponent extends Component
+{
+    public $user;
+    public $sender_id;
+    public $receiver_id;
+    public $message = '';
+
+    public function render()
+    {
+        return view('livewire.chat-component');
+    }
+
+    public function mount($user_id){
+        $this->sender_id = auth()->user()->id;
+        $this->receiver_id = auth()->user()->id;
+
+
+        $this->user = User::whereId($user_id)->first();
+    }
+
+    public function sendMessage(){
+        $chatMessage = new Message();
+        $chatMessage->sender_id = $this->sender_id;
+        $chatMessage->receiver_id = $this->receiver_id;
+        $chatMessage->message = $this->message;
+        $chatMessage->save();
+
+        $this->message = '';
+    }
+}
